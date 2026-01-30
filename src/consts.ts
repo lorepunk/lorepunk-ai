fbad0d6c9ba940686173d6b537e24437590c34dd
lorepunk <lorepunkdoteth@gmail.com>
2026-01-30 01:50:07 +0000

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "lorepunk";
+export const SITE_DESCRIPTION = "Writer. Narrative designer. Poet. Building stories at the intersection of words and worlds.";
